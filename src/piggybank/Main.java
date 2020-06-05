d78f54e7d7e983d7990f3698a8e67550fce4bb7a
Kate McGee <mcgeehee@gmail.com>
2020-06-05 09:09:54 -0600

    added piggy bank folder, main file and printing correctly

---
+++ src/piggybank/Main.java
@@ -0,0 +1,15 @@
+package piggybank; 
+
+import java.util.*; 
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("PIGGY BANK");
+    }
+
+}
+// compiling code:
+// inside src
+// javac folderNameUnderSrcFiles/*.java (compile)
+// jar cvfe folderNameUnderSrcFiles.jar folderNameUnderSrcFiles.Main folderNameUnderSrcFiles/*.class (make an archive)
+// java -jar foldernameUnderSrcFiles.jar (run it)
